060c03acac08a5d3e4057a74a6805d14467a061d
victorbamikole <victorbamikole92@gmail.com>
2023-04-03 17:12:15 +0100

    Firebase Auth

---
+++ screens/SignupScreen.js
@@ -0,0 +1,34 @@
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
+import { createUser } from "../util/auth";
+import { useContext, useState } from "react";
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext)
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, check your input and try again"
+      );
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Creating user..."} />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
